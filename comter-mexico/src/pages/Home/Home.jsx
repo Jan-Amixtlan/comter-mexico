54165240f5c5059c7da9ca398e958f9e75242bd6
Jan-Amixtlan <janethamixtlan127@gmail.com>
2025-10-08 15:51:41 -0600

    feat: Implement HeroSection component with styling and animations

--- comter-mexico/src/pages/Home/Home.jsx
+++ comter-mexico/src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import Hero from "../../components/NavbarHero/Hero";
+import HeroSection from "../../components/HeroSection/HeroSection";
 
 const Home = () => {
     return (
-        <div>
+        <div style={{ margin: 0, padding: 0, minHeight: '100vh' }}>
+            <HeroSection />
             <Hero />
+
             {/* Otros componentes de la página de inicio */}
         </div>
     );
